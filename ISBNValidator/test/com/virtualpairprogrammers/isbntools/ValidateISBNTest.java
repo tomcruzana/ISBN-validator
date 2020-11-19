e1a290d1bbf42ca8c6b7f6e2de60bdf5060ff2aa
tomcruzana <tomcruzana@ymail.com>
2020-11-18 21:48:38 -0800

    Added StockManagement class and test suite
    Signed-off-by: tomcruzana <tomcruzana@ymail.com>

--- ISBNValidator/test/com/virtualpairprogrammers/isbntools/ValidateISBNTest.java
+++ ISBNValidator/test/com/virtualpairprogrammers/isbntools/ValidateISBNTest.java
@@ -2,70 +2,69 @@ package com.virtualpairprogrammers.isbntools;
 
 import static org.junit.Assert.*;
 
+import org.hamcrest.core.StringContains;
 import org.junit.Test;
 
 public class ValidateISBNTest {
 
 	@Test
 	public void checkAValid10digitISBN() {
-		//1st logic test
+		// 1st logic test
 		ValidateISBN validator = new ValidateISBN();
 		boolean result = validator.checkISBN("1789613477");
 		assertTrue("first isbn value test", result);
-		
-		//2nd identical logic test w/ different isbn
-		result = validator.checkISBN("1465475575");	
+
+		// 2nd identical logic test w/ different isbn
+		result = validator.checkISBN("1465475575");
 		assertTrue("second isbn value test", result);
 	}
-	
-	/*Test 13-digit ISBN*/
+
+	/* Test 13-digit ISBN */
 	@Test
 	public void checkAValid13digitISBN() {
-		//1st test logic
+		// 1st test logic
 		ValidateISBN validator = new ValidateISBN();
 		boolean result = validator.checkISBN("9781627516181");
 		assertTrue(result);
-		
-		//2nd identical test logic
+
+		// 2nd identical test logic
 		result = validator.checkISBN("9780321563842");
 		assertTrue(result);
 	}
-	
+
 	@Test
 	public void TenDigitISBNNumbersEndingInAnXAreValid() {
 		ValidateISBN validator = new ValidateISBN();
 		boolean result = validator.checkISBN("012000030X");
 		assertTrue("isbn with x test", result);
 	}
-	
+
 	@Test
 	public void checkAnInvalid10digitISBN() {
 		ValidateISBN validator = new ValidateISBN();
 		boolean result = validator.checkISBN("1789613476");
 		assertFalse(result);
 	}
-	
+
 	@Test
 	public void checkAnInvalid13digitISBN() {
 		ValidateISBN validator = new ValidateISBN();
 		boolean result = validator.checkISBN("9781627516186");
 		assertFalse(result);
 	}
-	
-	//JUnit 4 exception checking
+
+	// JUnit 4 exception checking
 	@Test(expected = NumberFormatException.class)
 	public void nineDigitISBNsAreNotAllowed() {
 		ValidateISBN validator = new ValidateISBN();
 		validator.checkISBN("123456789");
 	}
-	
+
 	@Test(expected = NumberFormatException.class)
 	public void nonNumericISBNsAreNotAllowed() {
 		ValidateISBN validator = new ValidateISBN();
 		validator.checkISBN("helloworld");
-		
-	}
-	
 
+	}
 
 }
