6ff689eaea1cf00962f07f95d79e60c32a38c2f0
tomcruzana <tomcruzana@ymail.com>
2020-11-18 20:15:10 -0800

    improved and refactored ValidateISBN class
    Signed-off-by: tomcruzana <tomcruzana@ymail.com>

--- ISBNValidator/src/com/virtualpairprogrammers/isbntools/ValidateISBN.java
+++ ISBNValidator/src/com/virtualpairprogrammers/isbntools/ValidateISBN.java
@@ -2,49 +2,65 @@ package com.virtualpairprogrammers.isbntools;
 
 public class ValidateISBN {
 
+	private static final int LONG_ISBN_MULTIPLIER = 10;
+	private static final int SHORT_ISBN_MULTIPLIER = 11;
+	private static final int SHORT_ISBN_LENGTH = 10;
+	private static final int LONG_ISBN_LENGTH = 13;
+
 	public boolean checkISBN(String isbn) {
 
 		// check if isbn digits is 13
-		if (isbn.length() == 13) {
-			int total = 0;
+		if (isbn.length() == LONG_ISBN_LENGTH) {
+			return isThisAValidLongISBN(isbn);
 
-			for (int i = 0; i < 13; i++) {
-				if (i % 2 == 0) { // check if even or odd
-					total += Character.getNumericValue(isbn.charAt(i)); // if even
-				} else {
-					total += Character.getNumericValue(isbn.charAt(i)) * 3; // if odd
-				}
-			}
-			return (total % 10 == 0) ? true : false; // check if divisible by 10
+		}
+		// check if isbn digits is 10
+		else if (isbn.length() == SHORT_ISBN_LENGTH) {
+			return isThisAValidShortISBN(isbn);
 
-		} else {
-			// check if isbn digits is 10
-			if (isbn.length() != 10) {
-				throw new NumberFormatException("ISBN numbers must be 10 digits long.");
-			}
-
-			int total = 0;
-
-			for (int i = 0; i < 10; i++) {
-				if (!Character.isDigit(isbn.charAt(i))) {
-					// check if last char is X
-					if (i == 9 && isbn.charAt(i) == 'X') {
-						// This is okay, and we need to add 10 as per ISBN calculation
-						total += 10;
-						// if index is 9 (which is the last digit AND if the last digit/char is capital
-						// X
-					} else {
-						throw new NumberFormatException("ISBN numbers must be numeric");
-					}
-				}
-				total += Character.getNumericValue(isbn.charAt(i)) * (10 - i); // convert the charAt to an int numeric
-																				// value and multiply by 10 - 1
-
-			}
-
-			return (total % 11 == 0) ? true : false; // check if divisible by 11
 		}
 
+		throw new NumberFormatException("ISBN numbers must be 10 or 13 digits long.");
+
+	}
+
+	private boolean isThisAValidShortISBN(String isbn) {
+		int total = 0;
+
+		for (int i = 0; i < SHORT_ISBN_LENGTH; i++) {
+			if (!Character.isDigit(isbn.charAt(i))) {
+				// check if last char is X
+				if (i == 9 && isbn.charAt(i) == 'X') {
+					// This is okay, and we need to add 10 as per ISBN calculation
+					total += 10;
+					// if index is 9 (which is the last digit AND if the last digit/char is capital
+					// X
+				} else {
+					throw new NumberFormatException("ISBN numbers must be numeric");
+				}
+			}
+			/*
+			 * convert the charAt to an int numeric value and multiply by 10 - 1
+			 */
+
+			total += Character.getNumericValue(isbn.charAt(i)) * (SHORT_ISBN_LENGTH - i);
+
+		}
+
+		return (total % SHORT_ISBN_MULTIPLIER == 0) ? true : false; // check if divisible by 11
+	}
+
+	private boolean isThisAValidLongISBN(String isbn) {
+		int total = 0;
+
+		for (int i = 0; i < LONG_ISBN_LENGTH; i++) {
+			if (i % 2 == 0) { // check if even or odd
+				total += Character.getNumericValue(isbn.charAt(i)); // if even
+			} else {
+				total += Character.getNumericValue(isbn.charAt(i)) * 3; // if odd
+			}
+		}
+		return (total % LONG_ISBN_MULTIPLIER == 0) ? true : false; // check if divisible by 10
 	}
 
 }
